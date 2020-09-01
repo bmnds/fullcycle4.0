c559a81ade606d45ee6cab9ec89485710dc27ff3
Bruno Mendes <bruno.mendes@prodam.am.gov.br>
2020-08-31 22:11:36 -0400

    GitHub Actions on React App

--- desafio4/src/App.test.tsx
+++ desafio4/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders users list page by default', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Users/);
   expect(linkElement).toBeInTheDocument();
 });
